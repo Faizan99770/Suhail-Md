8ef21787149e08e92ef92f31247e236dc23c34af
Faizan99770 <alianjutt162@gmail.com>
2024-05-29 10:36:00 +0500

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ global.email ="saimsamsun789@gmail.com"
 global.location="Lahore,Pakistan."
 
 
-global.mongodb= process.env.MONGODB_URI || "SuhailTechInfo/Suhail-Md"
+global.mongodb= process.env.MONGODB_URI || ":Faizan99770/Suhail-Md Mongob"
 global.allowJids= process.env.ALLOW_JID || "null" 
 global.blockJids= process.env.BLOCK_JID || "null"
 global.DATABASE_URL = process.env.DATABASE_URL || ""
